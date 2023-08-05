af2b4974fba26d595de7400819f6eed17c0a0d0f
Matt Crow <mattcrow19@gmail.com>
2023-08-05 11:49:57 -0700

    .NET started template

---
+++ GearBox.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace GearBox.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(4, Class1.Add(2, 2));
+    }
+}
---
+++ GearBox.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace GearBox.Core;
+
+public class Class1
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
