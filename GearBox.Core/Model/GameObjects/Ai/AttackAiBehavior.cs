55a4f7a3a8c8c2cbb956f0ee56cb3d6ef80c8bde
Matt Crow <mattcrow19@gmail.com>
2024-07-20 14:06:03 -0700

    Finish adding AreaChanged event to players for #91

--- GearBox.Core/Model/GameObjects/Ai/AttackAiBehavior.cs
+++ GearBox.Core/Model/GameObjects/Ai/AttackAiBehavior.cs
@@ -1,3 +1,5 @@
+using GearBox.Core.Model.Areas;
+using GearBox.Core.Model.GameObjects.Player;
 using GearBox.Core.Model.Units;
 
 namespace GearBox.Core.Model.GameObjects.Ai;
@@ -11,6 +13,23 @@ public class AttackAiBehavior : IAiBehavior
     {
         _controlling = controlling;
         _attacking = attacking;
+
+        /* 
+            While I don't like this cast, it's required so I can uphold a few rules:
+            1. players can change areas, but characters cannot (cannot push AreaChanged to superclass)
+            2. characters controlled by AI can attack other characters controlled by AI (cannot pull attacking to subclass)
+            3. AttackAiBehavior is instantiated by PursueAiBehavior, which also targets characters (cannot overload ctor)
+        */
+        if (attacking is PlayerCharacter player)
+        {
+            player.AreaChanged += HandleAreaChangedEvent;
+        }
+    }
+
+    private void HandleAreaChangedEvent(object? sender, AreaChangedEventArgs e)
+    {
+        _controlling.AiBehavior = new WanderAiBehavior(_controlling);
+        e.WhoChangedAreas.AreaChanged -= HandleAreaChangedEvent;
     }
 
     public void Update()
