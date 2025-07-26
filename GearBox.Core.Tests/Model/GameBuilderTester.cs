c7ff67be403032790ee3803628e9730f02a69384
Matt Crow <mattcrow19@gmail.com>
2025-07-26 07:46:56 -0700

    Fix tests broken by last commit

--- GearBox.Core.Tests/Model/GameBuilderTester.cs
+++ GearBox.Core.Tests/Model/GameBuilderTester.cs
@@ -1,5 +1,6 @@
 using GearBox.Core.Config;
 using GearBox.Core.Model;
+using GearBox.Core.Utils;
 using Xunit;
 
 namespace GearBox.Core.Tests.Model;
@@ -9,7 +10,7 @@ public class GameBuilderTester
     [Fact]
     public void AreaNameMustBeUnique()
     {
-        var sut = new GameBuilder(new GearBoxConfig())
+        var sut = new GameBuilder(new GearBoxConfig(), new RandomNumberGenerator())
             .WithArea("foo", 1, area => area);
         
         Assert.Throws<ArgumentException>(() => sut.WithArea("foo", 1, area => area));
